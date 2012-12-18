471db5d86b1c0cfba15a1bbe0b51f19f4625c653
Steffen Hartmann <info@fruityweb.de>
2012-12-18 12:57:47 +0100

    first working version

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 vagrant-solr4
 =============
+
+Vagrant template for creating a simple solr4 virtual box based on the 'lucid64' base box
+
+1. Basic Installation
+```
+vagrant up
+```
+2. Post Installation Steps
+
+*Change directory owner
+```
+sudo chmod /etc/solr/data/
+sudo chmod /etc/solr/zoo_data/
+```
+
+*Edit Solr/Tomcat configuration
+
+Change the following entry in file /etc/solr/collection1/conf/solrconfig.xml
+```
+<dataDir>${solr.data.dir:/etc/solr/data}</dataDir>
+```
+
+Create the following file /etc/tomcat6/Catalina/localhost/solr.xml
+```
+<?xml version="1.0" encoding="utf-8"?>
+<Context docBase="/etc/solr/solr.war" debug="0" crossContext="true">
+  <Environment name="solr/home" type="java.lang.String" value="/etc/solr" override="true"/>
+</Context>
+```
+
+*Optional: Adding SolrCloud configuration
+
+For main cloud server add the following line to the tomcat startup script
+```
+export JAVA_OPTS="$JAVA_OPTS -Dbootstrap_confdir=/etc/solr/collection1/conf -Dcollection.configName=myconf -DzkRun -DnumShards=2"
+```
+
+For additional cloud server add the following line to the tomcat startup script
+```
+-DzkHost=<zk-host>:9983
+```
+
+*Additional information
+http://wiki.apache.org/solr/SolrCloud
+http://wiki.apache.org/solr/NewSolrCloudDesign
--- manifests/default.pp
+++ manifests/default.pp
@@ -1,31 +1,92 @@
 # Basic Puppet Apache manifest
 
-class apache {
+# installing updates and additional tools
+class system {
+	
   exec { 'apt-get update':
     command => '/usr/bin/apt-get update'
   }
 
-  package { "apache2":
-    ensure => present,
+  package { "vim":
+  	ensure => present,
   }
 
+  package { "curl":
+  	ensure => present, 
+  }
+
+}
+
+# setting up apache2 as service
+class apache {
+
+  package { "apache2":
+  	ensure => present,
+  }
+  
   service { "apache2":
     ensure => running,
     require => Package["apache2"],
   }
+
 }
 
-class solr {
-  
-  package { "solr-tomcat":
-    ensure => present,
-  }
+# setting up tomcat as service
+class tomcat {
 
+  package { "tomcat6":
+  	ensure => present,
+  }
+  
   service { "tomcat6":
     ensure => running,
-    require => Package["solr-tomcat"],
+    require => Package["tomcat6"],
   }
+
 }
 
-#include apache
-#include solr
+# setting up solr4 
+class solr {
+
+	exec { "solr-download":
+		command => "wget http://apache.mirror.iphh.net/lucene/solr/4.0.0/apache-solr-4.0.0.tgz",
+		cwd => "/home/vagrant/",
+		creates => "/home/vagrant/apache-solr-4.0.0.tgz",
+		path => ["/usr/bin", "/usr/sbin/"]
+	}
+
+	exec { "solr-inflate":
+		command => "tar xzf apache-solr-4.0.0.tgz",
+		cwd => "/home/vagrant/",
+		creates => "/home/vagrant/apache-solr4/",
+		path => "/bin/",
+		require => Exec["solr-download"]
+	}
+
+	exec { "solr-createdirectory":
+		command => "mkdir /etc/solr/",
+		cwd => "/home/vagrant/",
+		path => ["/usr/bin", "/usr/sbin/", "/bin/"],
+	}
+
+	exec { "solr-createzoodirectory":
+		command => "mkdir /etc/solr/zoo_data/",
+		cwd => "/home/vagrant/",
+		path => ["/usr/bin", "/usr/sbin/", "/bin/"],
+
+	}
+
+	exec { "solr-install":
+		command => "cp -R apache-solr-4.0.0/example/solr/* /etc/solr/ | cp -R apache-solr-4.0.0/example/webapps/solr.war /etc/solr/",
+		cwd => "/home/vagrant/",
+		path => ["/usr/bin", "/usr/sbin/", "/bin"],
+		require => Exec["solr-inflate"]
+	}
+
+}
+
+
+include system
+include apache
+include tomcat
+include solr
